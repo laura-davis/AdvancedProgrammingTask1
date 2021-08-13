f7f6818600eaccb5b17e1f86ae1b381b8b266e09
laura-davis <laura.davis@sky.uk>
2021-08-13 14:23:04 +0100

    Task 1 complete

---
+++ cmake-build-debug/CMakeFiles/AdvancedProgrammingTask1.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "AdvancedProgrammingTask1"
+  "AdvancedProgrammingTask1.pdb"
+  "CMakeFiles/AdvancedProgrammingTask1.dir/DrawableGameComponent.cpp.o"
+  "CMakeFiles/AdvancedProgrammingTask1.dir/Game.cpp.o"
+  "CMakeFiles/AdvancedProgrammingTask1.dir/GameComponent.cpp.o"
+  "CMakeFiles/AdvancedProgrammingTask1.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AdvancedProgrammingTask1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
